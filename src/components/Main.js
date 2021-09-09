4e7201b8e3be46d90b8c538abdbcbb8e43e2312c
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-09-09 12:50:06 -0400

    fixed browser

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { MemoryRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import Blog from '../components/Blog/Blog'
 import Resume from './Resume'
 import Homepage from './Homepage';
