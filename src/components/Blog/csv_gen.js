aed4fae0da37e857c5f00a6e3588850ba28444ca
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-09-14 14:09:01 -0400

    added github link

--- src/components/Blog/csv_gen.js
+++ src/components/Blog/csv_gen.js
@@ -22,14 +22,13 @@ const CsvGen = () => {
                     <p className="tab">This function was a later addition. It is not a big change, nor does it take full advantage of Python's functionality or PANDAS. However, it does enable a user export data in the PostgreSql to their own computer</p>
                     <img className="fe_image d-flex justify-content-center" src={CsvGif} />
                     <p className="tab">As the above gif demonstrates, all that the user needs to do is to click the "Generate CSV" button in the NavBar to start the process</p>
-                    <a href="https://github.com/JpBongiovanni/WorldDeityDatabase/blob/main/wiki_app/views.py">GitHub</a>
                     <img className="fe_image d-flex justify-content-center" src={CsvImg} />
                     <p className="tab">
                         Once the program has run its course, the CSV can be opened, manipulated, imported to another database, and saved.
                     </p>
                     <img className="fe_image d-flex justify-content-center" src={CsvCode} />
                     <p className="tab">
-                        The function in views.py returns an HttpResponse that takes the content type of "text/csv." Because every column in my database is not really necessary to export (like contributor id, or upload date), the function writes rows based on the selected columns, skipping over the ones not selected. 
+                        The function in views.py returns an HttpResponse that takes the content type of "text/csv." Because every column in my database is not really necessary to export (like contributor id, or upload date), the function writes rows based on the selected columns, skipping over the ones not selected. Check out the full code on <a href="https://github.com/JpBongiovanni/WorldDeityDatabase/blob/main/wiki_app/views.py"><strong>GitHub</strong></a>.
                     </p>
                 </div>
             </div>
