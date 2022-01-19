3334c34e941c128fff18e564ebbbd4955ed2840a
Jason Bongiovanni <jpbongiovanni@gmail.com>
2022-01-19 16:35:27 -0500

    update blog message

--- src/components/Blog/BlogApp.js
+++ src/components/Blog/BlogApp.js
@@ -23,7 +23,7 @@ function BlogApp() {
         <section className="menu section">
             <div className="title">
                 <h2>Entries</h2>
-                
+                <h4>Update coming soon - March 2022</h4>
                 <Categories categories={categories}filterItems={filterItems}/>
             </div>
             <div>
